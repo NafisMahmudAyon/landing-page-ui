b0a99ac219e843ab83e039113f0f0013442ac245
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-02-26 19:59:14 +0600

    publish v1.0.1

--- src/index.jsx
+++ src/index.jsx
@@ -8,59 +8,59 @@ import imagesLoaded from "imagesloaded";
 
 
 ///////
-import Text from "./Text";
+// import Text from "./Text";
 
 
 // *css
-import "./output.css";
+import "../css/output.css";
 
 
 
 
 // * Text
-// const Text = ({
-// 	style = "",
-// 	tagName,
-// 	isLink,
-// 	linkTo,
-// 	target,
-// 	children,
-// 	variant,
-// }) => {
-// 	const [customTag, setCustomTag] = useState(tagName || "div");
-// 	const [variantValue, setVariantValue] = useState("");
+const Text = ({
+	style = "",
+	tagName,
+	isLink,
+	linkTo,
+	target,
+	children,
+	variant,
+}) => {
+	const [customTag, setCustomTag] = useState(tagName || "div");
+	const [variantValue, setVariantValue] = useState("");
 
-// 	useEffect(() => {
-// 		if (isLink) {
-// 			setCustomTag("a");
-// 		} else {
-// 			setCustomTag(tagName || "div");
-// 		}
-// 	}, [isLink, tagName]);
+	useEffect(() => {
+		if (isLink) {
+			setCustomTag("a");
+		} else {
+			setCustomTag(tagName || "div");
+		}
+	}, [isLink, tagName]);
 
-// 	useEffect(() => {
-// 		if (variant == "1") {
-// 			setVariantValue(
-// 				"relative text-3xl font-bold  cursor-pointer transition hover:bg-blue-900 duration-500 ease-in-out hover:text-blue-500  hover:after:w-full after:absolute after:-bottom-2 after:left-0 after:w-2 after:h-2 after:bg-blue-500 after:transition-all after:duration-500 after:ease-in-out "
-// 			);
-// 		}
-// 	}, [variant]);
+	useEffect(() => {
+		if (variant == "1") {
+			setVariantValue(
+				"relative text-3xl font-bold  cursor-pointer transition hover:bg-blue-900 duration-500 ease-in-out hover:text-blue-500  hover:after:w-full after:absolute after:-bottom-2 after:left-0 after:w-2 after:h-2 after:bg-blue-500 after:transition-all after:duration-500 after:ease-in-out "
+			);
+		}
+	}, [variant]);
 
-// 	const CustomTag = customTag.toLowerCase();
+	const CustomTag = customTag.toLowerCase();
 
-// 	return (
-// 		<CustomTag
-// 			className={`${style} ${variantValue}  block`}
-// 			{...(isLink && {
-// 				href: linkTo,
-// 				target: target,
-// 			})}
-// 			// dangerouslySetInnerHTML={{ __html: content }}
-// 		>
-// 			{children}
-// 		</CustomTag>
-// 	);
-// };
+	return (
+		<CustomTag
+			className={`${style} ${variantValue}  block`}
+			{...(isLink && {
+				href: linkTo,
+				target: target,
+			})}
+			// dangerouslySetInnerHTML={{ __html: content }}
+		>
+			{children}
+		</CustomTag>
+	);
+};
 
 
 
