7af005bd892cc025db49a6318c66d0f0d94814ef
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-04-23 19:34:55 +0600

    publish v2.0.3

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import React, { useEffect, useState, useRef, Children } from "react";
 // import imagesLoaded from "imagesloaded";
 
 // * package for code snippets
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+// import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 ///////
 // import Text from "./Text";
@@ -6561,100 +6561,6 @@ const Badge = ({
 	);
 };
 
-// * CodeSnippet
-const CodeSnippet = ({
-	content,
-	lang = "html",
-	styles = "",
-	headerStyles = "",
-	bodyStyles = "",
-}) => {
-	const [copySuccess, setCopySuccess] = useState(null);
-
-	const handleCopyClick = () => {
-		navigator.clipboard
-			.writeText(content.trim())
-			.then(() => setCopySuccess(true))
-			.catch(() => setCopySuccess(false));
-
-		// Reset copy success message after 2 seconds
-		setTimeout(() => {
-			setCopySuccess(null);
-		}, 2000);
-	};
-	return (
-		<Code styles={` ${styles}  rounded-t-lg rounded-b-lg relative`}>
-			<CodeHeader
-				styles={` ${headerStyles} flex items-center justify-between   p-2 w-full bg-[#b4b4b4] text-white rounded-t-lg pl-4 `}>
-				<Text styles="">{lang}</Text>
-				<IconButton
-					tagName="button"
-					textOnClick={handleCopyClick}
-					icon="fa-copy"
-					iconLibrary="font-awesome"
-					iconStyles="mr-2"
-					text={
-						copySuccess === null
-							? "Copy code"
-							: copySuccess === true
-							? "Code copied"
-							: "Failed to copy"
-					}
-					styles="absolute top-0 right-0 p-2 text-inherit z-10 pr-4 cursor-pointer "
-				/>
-			</CodeHeader>
-			<CodeBody
-				content={content}
-				language={lang}
-				styles={` ${bodyStyles} pt-1 px-4 pb-1 text-sm overflow-y-scroll block  `}
-			/>
-		</Code>
-	);
-};
-
-// * Code
-const Code = ({
-	// style,
-	content,
-	tagName = "pre",
-	languages = "css",
-	styles = "",
-	children,
-}) => {
-	const [customTag, setCustomTag] = useState(tagName || "pre");
-	const CustomTag = customTag.toLowerCase();
-
-	return (
-		<CustomTag className={` ${styles} `}>
-			{/* <SyntaxHighlighter language={languages} styles={styles || "vscDarkPlus"}>
-				{content}
-			</SyntaxHighlighter> */}
-			{children}
-		</CustomTag>
-	);
-};
-
-// * CodeHeader
-const CodeHeader = ({ tagName, styles = "", children }) => {
-	const [customTag, setCustomTag] = useState(tagName || "div");
-	const CustomTag = customTag.toLowerCase();
-	return <CustomTag className={` ${styles} `}>{children}</CustomTag>;
-};
-
-// * CodeBody
-const CodeBody = ({ tagName, styles = "", language, content }) => {
-	const [customTag, setCustomTag] = useState(tagName || "code");
-	const CustomTag = customTag.toLowerCase();
-	return (
-		<SyntaxHighlighter
-			className={` ${styles} `}
-			language={language}
-			style={vscDarkPlus}>
-			{content}
-		</SyntaxHighlighter>
-	);
-};
-
 // * Divider
 const Divider = ({
 	tagName,
@@ -7929,10 +7835,6 @@ export {
 	AvatarGroup,
 	Avatar,
 	Badge,
-	CodeSnippet,
-	Code,
-	CodeHeader,
-	CodeBody,
 	Divider,
 	Card,
 	CardContent,
