510467b1172f0c196e9436f8493c2245f5dea076
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-06-02 20:25:23 +0600

    publish v2.1.33

---
+++ app/src/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
