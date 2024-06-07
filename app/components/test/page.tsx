ddfd3af6de6fa525765478ad63b969d75ae97236
AYON <nafismahmudayon@gmail.com>
2024-06-07 10:45:02 +0600

    publish v2.1.38

--- app/components/test/page.tsx
+++ app/components/test/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Accordion, AccordionHeader, Text, AccordionDetails, Icon } from '@/app/src'; // adjust the import paths
+import { Accordion, AccordionHeader, Text, AccordionDetails } from '@/app/src'; // adjust the import paths
 
 const Page = () => {
   
